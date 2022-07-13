0a7881e6eb3ae1d2c5f90e488ebc4b90c4415159
Leandro Cabral <enigmha@gmail.com>
2022-07-13 00:31:32 -0300

    Outros Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import NotiticationButton from './components/NotificationButton'
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Teste Metas</h1>
-      <NotiticationButton />
-      <NotiticationButton />
-      <NotiticationButton />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
